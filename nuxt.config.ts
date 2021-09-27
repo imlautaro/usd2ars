1052dfc33dad836d16ddaac27360a1f5867f64c8
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-09-27 20:18:33 -0300

    add seo stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,13 +17,27 @@ const config: NuxtConfig = {
 		},
 	},
 	head: {
+		link: [
+			{
+				rel: 'canonical',
+				href: 'https://usd2ars.nuxt.club/',
+			},
+		],
 		script: [{ src: 'https://unpkg.com/phosphor-icons' }],
 	},
 	modules: ['@nuxt/http'],
 	pwa: {
+		meta: {
+			name: 'Convertir dólar a peso argentino | Oficial, Blue y Turista',
+			description:
+				'Mirá los diversos valores del dólar hoy. Hacé la conversión al instante de USD a ARS o ARS a USD. Instalá la app gratis y sin anuncios ✅.',
+			ogHost: 'https://usd2ars.nuxt.club',
+			ogImage: '/featured.png',
+		},
 		manifest: {
 			name: 'USD2ARS',
 			short_name: 'USD2ARS',
+			lang: 'es',
 		},
 	},
 	router: {
